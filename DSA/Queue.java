eae68d9718403a9e53b015b43eac8b061a212d30
Sahana Mahendra <89964115+SahanaMahendra@users.noreply.github.com>
2024-07-06 23:47:55 -0400

    Added enQueue and deQueue method

--- DSA/Queue.java
+++ DSA/Queue.java
@@ -24,5 +24,32 @@ public class Queue {
         return length == 0;
     }
 
+    private void enQueue(int data){
+        ListNode temp= new ListNode(data);
+        if (isEmpty()){
+            front=temp;
+        }
+        else{
+            rare.next=temp;
+        }
+        rare=temp;
+        length++;
+    }
+
+    private int deQueue(){
+        int result;
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        else{
+           result= front.data;
+           front=front.next;
+           if(front==null){
+               rare=null;
+           }
+        }
+        length--;
+        return result;
+    }
 
 }
