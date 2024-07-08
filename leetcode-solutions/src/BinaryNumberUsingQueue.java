7156079cc3044de2bb64490a3c25007f4ca122b3
“SahanaMahendra” <sahanamahendra1996@gmail.com>
2024-07-07 23:08:47 -0400

    solution to find Binary Number Using Queues

--- leetcode-solutions/src/BinaryNumberUsingQueue.java
+++ leetcode-solutions/src/BinaryNumberUsingQueue.java
@@ -1,2 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryNumberUsingQueue {
+    static String[] binaryNumberUsingQueue(int n){
+        String[] result =new String[n];
+        Queue<String> que= new LinkedList<>();
+        que.offer("1");
+        for(int i=1; i<n; i++){
+            result[i]=que.poll();
+            String n1=result[i]+"0";
+            String n2=result[i]+"1";
+            que.offer(n1);
+            que.offer(n2);
+        }
+        return result;
+    }
 }
