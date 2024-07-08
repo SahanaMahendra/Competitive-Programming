9280a21dd4992d549da82314a727be40a6e963c5
“SahanaMahendra” <sahanamahendra1996@gmail.com>
2024-07-07 22:34:20 -0400

    solution to number of ones

--- leetcode-solutions/src/NumberOfOneBits.java
+++ leetcode-solutions/src/NumberOfOneBits.java
@@ -1,2 +1,12 @@
 public class NumberOfOneBits {
-}
+        public int hammingWeight(int n) {
+            int res = 0;
+            for (int i = 0; i < 32; i++) {
+                if (((n >> i) & 1) == 1) {
+                    res += 1;
+                }
+            }
+            return res;
+        }
+    }
+
