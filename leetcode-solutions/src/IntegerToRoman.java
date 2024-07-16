a012a416ee170dd34d44c33ef4ee6983e68fdc38
“SahanaMahendra” <sahanamahendra1996@gmail.com>
2024-07-16 14:10:19 -0400

    /IntegerToRoman.java

--- leetcode-solutions/src/IntegerToRoman.java
+++ leetcode-solutions/src/IntegerToRoman.java
@@ -1,16 +1,21 @@
+import java.util.Arrays;
+
 public class IntegerToRoman {
 
     public static String intToRoman(int num) {
         int[]    val={1000, 900, 500, 400, 100,  90,   50, 40,  10,    9,   5,    4,   1};
-        String[] str={"M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV", "I"};
-        String res="";
-        for(int i=0; i<val.length; i++){
-            while(num>=val[i]){
-                num-=val[i];
-                res+=str[i];
-            }
-        }
-        return res;
+        Arrays.stream(val).filter(x -> x > 500).forEach(System.out::println);
+//        System.out.println();
+//
+//        String[] str={"M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV", "I"};
+//        String res="";
+//        for(int i=0; i<val.length; i++){
+//            while(num>=val[i]){
+//                num-=val[i];
+//                res+=str[i];
+//            }
+//        }
+        return null;
    }
 
    public static void main(String[] args){
